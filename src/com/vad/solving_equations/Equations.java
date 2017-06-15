ceef8281ecb752a18e0ee9c1754293f372f671fd
vadim <gabderahmanov99@gmail.com>
2017-06-15 13:07:54 +0400

    add new method sloving equation

--- src/com/vad/solving_equations/Equations.java
+++ src/com/vad/solving_equations/Equations.java
@@ -2,88 +2,111 @@ package com.vad.solving_equations;
 
 public class Equations {
 	// a+x=b
-		// x+a=b
-		public void solvSum(int a, int b) {
-			int x = b - a;
+	// x+a=b
+	public void solvSum(int a, int b) {
+		int x = b - a;
+		print(x);
+	}
+
+	// x^2*a+b+c=0
+	// D=b^2-4*a*c
+	// x1 = -b+sqrt(d)/2a
+	// x2 = -b-sqrt(d)/2a
+	public void solvR(int a, int b, int c) {
+		double d = b ^ 2 - 4 * a * c;
+		double x1 = Math.sqrt(d) - b;
+		double x2 = Math.sqrt(d) + b * -1;
+
+		if (d < 0) {
+			print("Not Solution " + d);
+		}
+
+		print("Dis :" + d);
+		print(x1);
+		print(x2);
+	}
+
+	// x/b=a
+	public void slovFra(int a, int b) {
+		int x = b * a;
+
+		print(x);
+	}
+
+	// b/x=a
+	public void slovFra2(double a, double b) {
+		double x = a / b;
+
+		print(x);
+	}
+
+	// a*x=b
+	public void slovMul(double a, double b) {
+		double x = b / a;
+		print(x);
+	}
+
+	// a+x/b = c
+	public void slovFra3(double a, double b, double c) {
+		double phase_1 = c * -1;
+		double phase_2 = phase_1 * b;
+		double phase_3 = a + phase_2;
+		double x = phase_3 * -1;
+
+		print(x);
+	}
+
+	// a^2+b+c/d=e
+	public void slovFra4(int a, int b, int c, int d, int e) {
+		double phase_1 = e * -1;
+		double phase_2 = phase_1 * d;
+		int d_3 = (int) (c + phase_2);
+
+		double g = b ^ 2 - 4 * a * d_3;
+		double x1 = Math.sqrt(g) - b;
+		double x2 = (Math.sqrt(g) + b) * -1;
+
+		if (g < 0) {
+			print("Not dicision d<0 " + d);
+		}
+
+		print(x1);
+		print(x2);
+
+	}
+
+	// a/b+c/d=e;
+	public void slovFra5(int a, int b, int c, int d, int e) {
+		if (b != d) {
+			int phase_1 = e * -1;
+			int mul = b * d;
+			int ovrl = a * d;
+			int ovrl2 = c * b;
+			double phase_2 = ovrl + ovrl2 + phase_1;
+			double x = phase_2 / mul;
+
 			print(x);
-		}
 
-		// x^2*a+b+c=0
-		// D=b^2-4*a*c
-		// x1 = -b+sqrt(d)/2a
-		// x2 = -b-sqrt(d)/2a
-		public void solvR(int a, int b, int c) {
-			double d = b ^ 2 - 4 * a * c;
-			double x1 = Math.sqrt(d) - b;
-			double x2 = Math.sqrt(d) + b * -1;
-
-			if (d < 0) {
-				print("Not Solution " + d);
-			}
-
-			print("Dis :" + d);
-			print(x1);
-			print(x2);
-		}
-
-		// x/b=a
-		public void slovFra(int a, int b) {
-			int x = b * a;
+		} else {
+			int phase_1 = e * -1;
+			int phase_2 = phase_1 * d;
+			int phase_3 = a + c + phase_2;
+			int x = phase_3 / d;
 
 			print(x);
-		}
-
-		// b/x=a
-		public void slovFra2(double a, double b) {
-			double x = a / b;
-
-			print(x);
-		}
-
-		// a*x=b
-		public void slovMul(double a, double b) {
-			double x = b / a;
-			print(x);
-		}
-
-		// a+x/b = c
-		public void slovFra3(double a, double b, double c) {
-			double d_1 = c * -1;
-			double d_2 = d_1 * b;
-			double d_3 = a + d_2;
-			double x = d_3 * -1;
-
-			print(x);
-		}
-
-		// a^2+b+c/d=e
-		public void slovFra4(int a, int b, int c, int d, int e) {
-			double d_1 = e * -1;
-			double d_2 = d_1 * d;
-			int d_3 = (int) (c + d_2);
-
-			double g = b ^ 2 - 4 * a * d_3;
-			double x1 = Math.sqrt(g) - b;
-			double x2 = (Math.sqrt(g) + b) * -1;
-
-			if (g < 0) {
-				print("Not dicision d<0 " + d);
-			}
-			
-			print(x1);
-			print(x2);
 
 		}
+	}
 
-		private void print(int i) {
-			System.out.println(i);
-		}
+	private void print(int i) {
+		System.out.println(i);
+	}
 
-		private void print(double i) {
-			System.out.println(i);
-		}
+	private void print(double i) {
+		System.out.println(i);
+	}
 
-		private void print(String str) {
-			System.out.println(str);
-		}
+	private void print(String str) {
+		System.out.println(str);
+	}
 }
--- src/com/vad/solving_equations/Run.java
+++ src/com/vad/solving_equations/Run.java
@@ -4,6 +4,7 @@ public class Run {
 
 	public static void main(String[] args) {
 		Equations equations = new Equations();
+
 		equations.slovFra(2, 3);
 		equations.slovFra2(4, 5);
 		equations.slovFra3(5, 6, 7);
@@ -11,6 +12,7 @@ public class Run {
 		equations.slovMul(2, 6);
 		equations.solvR(1, -2, 3);
 		equations.solvSum(6, -5);
+		equations.slovFra5(1, 2, -7, 4, 3);
 
 	}
 
