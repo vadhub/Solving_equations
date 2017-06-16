d4fd1d870aae7236980ae52717643a343d61993d
vadim <gabderahmanov99@gmail.com>
2017-06-16 12:58:55 +0400

    add gui bloc

--- src/com/vad/solving_equations/Run.java
+++ src/com/vad/solving_equations/Run.java
@@ -1,9 +1,42 @@
 package com.vad.solving_equations;
+import java.awt.FlowLayout;
+
+import javax.swing.*;
 
 public class Run {
 
 	public static void main(String[] args) {
 		Equations equations = new Equations();
+		
+		//gui bloc
+		
+		JFrame frame = new JFrame();
+		
+		JTextField result = new JTextField();		
+		JButton enter = new JButton();
+		
+		JTextField a = new JTextField();
+		JTextField b = new JTextField();
+		JTextField c = new JTextField();
+		JTextField d = new JTextField();
+		JTextField e = new JTextField();
+		
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new FlowLayout());
+		
+		
+		panel1.add(enter);
+		panel1.add(a);
+		panel1.add(b);
+		panel1.add(c);
+		panel1.add(d);
+		panel1.add(e);
+		panel1.add(result);
+		
+		frame.add(panel1);
+		frame.setVisible(true);
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		equations.slovFra(2, 3);
 		equations.slovFra2(4, 5);
