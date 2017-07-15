bfb5e61bd141552da975b5d9e5db0193a810e74a
vadim <gabderahmanov99@gmail.com>
2017-07-15 12:53:38 +0400

    add method solvCtg_3!

--- src/com/vad/solvingequations/EquationsTrigeometry.java
+++ src/com/vad/solvingequations/EquationsTrigeometry.java
@@ -10,7 +10,7 @@ public class EquationsTrigeometry {
 
 	private int COUNT = 1;
 
-////////////////////sin special for display	
+	// //////////////////sin special for display
 	public String SpecialArcSin(double int_a) {
 
 		if (int_a == 1 / 2) {
@@ -41,10 +41,11 @@ public class EquationsTrigeometry {
 		} else if (int_a == -1) {
 			return "-pi/2 + pi*k, k=Z";
 		} else {
-			return "(-1)^k * arcsin(" + String.valueOf(int_a)+ ")+pi*k, k=Z";
+			return "(-1)^k * arcsin(" + String.valueOf(int_a) + ")+pi*k, k=Z";
 		}
 	}
-	////////////////////cos special for display
+
+	// //////////////////cos special for display
 	public String SpecialArcCos(double x) {
 
 		if (x == 1 / 2) {
@@ -80,8 +81,8 @@ public class EquationsTrigeometry {
 		}
 		return "ERROR";
 	}
-	
-	//////////////////////tg special for display 	
+
+	// ////////////////////tg special for display
 	public String SpecialArcTg(double x) {
 		if (x == 0) {
 			return "x = pi*n, n=Z";
@@ -140,7 +141,8 @@ public class EquationsTrigeometry {
 		} else if (int_a == -1) {
 			return "x = -pi/2 + pi*k, k=Z";
 		} else {
-			return "x = (-1)^k * arcsin(" + String.valueOf(int_a)+ ")+pi*k, k=Z";
+			return "x = (-1)^k * arcsin(" + String.valueOf(int_a)
+					+ ")+pi*k, k=Z";
 		}
 
 	}
@@ -376,15 +378,18 @@ public class EquationsTrigeometry {
 	// sin(x/a)=b
 	public String solvSin_3(double a, double b) {
 		if (b == 1 / 2) {
-			return "x = (-1)^k * " + a + " * " + angls[0] + " + " + a+ " * pi*k, k=Z";
+			return "x = (-1)^k * " + a + " * " + angls[0] + " + " + a
+					+ " * pi*k, k=Z";
 
 		} else if (b == Math.sqrt(2) / 2) {
 
-			return "x = (-1)^k * " + a + " * " + angls[1] + "+" + a	+ " * pi*k, k=Z";
+			return "x = (-1)^k * " + a + " * " + angls[1] + "+" + a
+					+ " * pi*k, k=Z";
 
 		} else if (b == Math.sqrt(3) / 2) {
 
-			return "x = (-1)^k * " + a + " * " + angls[2] + " + " + a+ " * pi*k, k=Z";
+			return "x = (-1)^k * " + a + " * " + angls[2] + " + " + a
+					+ " * pi*k, k=Z";
 		} else if (b == 1) {
 
 			return "x = " + a + " * " + angls[3] + "+" + a + "* pi*k, k=Z";
@@ -393,84 +398,124 @@ public class EquationsTrigeometry {
 			return "x = " + a + " * pi*k, k=Z";
 		} else if (b == -1 / 2) {
 
-			return "x = (-1)^k * -(" + String.valueOf(a * -1) + " * "+ neg_angls[1] + ") + " + a + " * pi*k, k=Z";
+			return "x = (-1)^k * -(" + String.valueOf(a * -1) + " * "
+					+ neg_angls[1] + ") + " + a + " * pi*k, k=Z";
 		} else if (b == -(Math.sqrt(2) / 2)) {
 
 			return "x = (-1)^k * -(" + String.valueOf(a * -1) + " * "
 					+ neg_angls[2] + ") + " + a + " * pi*k, k=Z";
 		} else if (b == -(Math.sqrt(3) / 2)) {
 
-			return "x = (-1)^k * -(" + a + " * " + neg_angls[3] + ") + " + a+ " * pi*k, k=Z";
+			return "x = (-1)^k * -(" + a + " * " + neg_angls[3] + ") + " + a
+					+ " * pi*k, k=Z";
 		} else if (b == -1) {
-			return "x =" + String.valueOf(a * -1)+" + " + "pi/2 +" + a	+ " * pi*k, k=Z";
+			return "x =" + String.valueOf(a * -1) + " + " + "pi/2 +" + a
+					+ " * pi*k, k=Z";
 		} else {
-			return "x = (-1)^k * arcsin(" + String.valueOf(b * a) + ") + " + a	+ " * pi*k, k=Z";
+			return "x = (-1)^k * arcsin(" + String.valueOf(b * a) + ") + " + a
+					+ " * pi*k, k=Z";
 		}
 	}
-	
+
 	// cos(x/a)=b
-		public String solvCos_3(double a, double b) {
-			if (b == 1 / 2) {
-				return "x = +-"+String.valueOf(a)+" * " + angls[2] +" + "+String.valueOf(a*2)+ "pi*n, n=Z";
+	public String solvCos_3(double a, double b) {
+		if (b == 1 / 2) {
+			return "x = +-" + String.valueOf(a) + " * " + angls[2] + " + "
+					+ String.valueOf(a * 2) + "pi*n, n=Z";
 
-			} else if (b == Math.sqrt(2) / 2) {
+		} else if (b == Math.sqrt(2) / 2) {
 
-				return "x = +-"+String.valueOf(a)+" * " +angls[1]+" + "+String.valueOf(a*2)+ " pi*n, n=Z";
+			return "x = +-" + String.valueOf(a) + " * " + angls[1] + " + "
+					+ String.valueOf(a * 2) + " pi*n, n=Z";
 
-			} else if (b == Math.sqrt(3) / 2) {
+		} else if (b == Math.sqrt(3) / 2) {
 
-				return "x = +-" +String.valueOf(a)+" * " + angls[0] +" + "+String.valueOf(a*2)+  "pi*n, n=Z";
-			} else if (b == 1) {
+			return "x = +-" + String.valueOf(a) + " * " + angls[0] + " + "
+					+ String.valueOf(a * 2) + "pi*n, n=Z";
+		} else if (b == 1) {
 
-				return "x = "+String.valueOf(a)+"pi*n, n=z";
-			} else if (b == 0) {
+			return "x = " + String.valueOf(a) + "pi*n, n=z";
+		} else if (b == 0) {
 
-				return "x =" +String.valueOf(a)+" * " + angls[3] +" + "+String.valueOf(a*2)+  " + pi*n, n=Z";
-			} else if (b == -1 / 2) {
+			return "x =" + String.valueOf(a) + " * " + angls[3] + " + "
+					+ String.valueOf(a * 2) + " + pi*n, n=Z";
+		} else if (b == -1 / 2) {
 
-				return "x = +-" +String.valueOf(a*-1)+" * " + cos_neg_angls[2] +" + "+String.valueOf(a*2)+  " + pi*n, n=Z";
-			} else if (b == -(Math.sqrt(2) / 2)) {
+			return "x = +-" + String.valueOf(a * -1) + " * " + cos_neg_angls[2]
+					+ " + " + String.valueOf(a * 2) + " + pi*n, n=Z";
+		} else if (b == -(Math.sqrt(2) / 2)) {
 
-				return "x = +-" +String.valueOf(a*-1)+" * " + cos_neg_angls[1] +" + "+String.valueOf(a*2)+  " +  pi*n, n=Z";
-			} else if (b == -(Math.sqrt(3) / 2)) {
+			return "x = +-" + String.valueOf(a * -1) + " * " + cos_neg_angls[1]
+					+ " + " + String.valueOf(a * 2) + " +  pi*n, n=Z";
+		} else if (b == -(Math.sqrt(3) / 2)) {
 
-				return "x = +-" +String.valueOf(a*-1)+" * " +cos_neg_angls[0] +" + "+String.valueOf(a*2)+  " +  pi*n, n=Z";
-			} else if (b == -1) {
+			return "x = +-" + String.valueOf(a * -1) + " * " + cos_neg_angls[0]
+					+ " + " + String.valueOf(a * 2) + " +  pi*n, n=Z";
+		} else if (b == -1) {
 
-				return "x = " +String.valueOf(a*-1)+" * " + cos_neg_angls[4] +" + "+String.valueOf(a*2)+ " pi*n, n=Z";
-			} else {
-				return "x = +- arccos(" + String.valueOf(a*b) + ")"+String.valueOf(a*2)+"+pi*k, k=Z";
-			}			
+			return "x = " + String.valueOf(a * -1) + " * " + cos_neg_angls[4]
+					+ " + " + String.valueOf(a * 2) + " pi*n, n=Z";
+		} else {
+			return "x = +- arccos(" + String.valueOf(a * b) + ")"
+					+ String.valueOf(a * 2) + "+pi*k, k=Z";
 		}
-		
-		//tg(x/a)=b	
-		public String solvTg_3(double a, double b) {
-			
-			if (b == 0) {
-				return "x ="+a+" * pi*n, n=Z";
-			} else if (b == 1 / (Math.sqrt(3))) {
+	}
 
-				return "x = " +a+" * "+ angls[0] +a+" * "+"pi*n, n=Z";
-			} else if (b == 1) {
+	// tg(x/a)=b
+	public String solvTg_3(double a, double b) {
 
-				return "x = " +a+" * "+ angls[1] +a+" * "+ "pi*n, n=Z";
-			} else if (b == Math.sqrt(3)) {
+		if (b == 0) {
+			return "x =" + a + " * pi*n, n=Z";
+		} else if (b == 1 / (Math.sqrt(3))) {
 
-				return "x = " +a+" * "+ angls[2] +a+" * "+ "pi*n, n=Z";
-			} else if (b == -(Math.sqrt(3))) {
+			return "x = " + a + " * " + angls[0] + a + " * " + "pi*n, n=Z";
+		} else if (b == 1) {
 
-				return "x = " +a+" * "+ neg_angls[2] +a+" * "+ "pi*n, n=Z";
-			} else if (b == -(1 / (Math.sqrt(3)))) {
+			return "x = " + a + " * " + angls[1] + a + " * " + "pi*n, n=Z";
+		} else if (b == Math.sqrt(3)) {
 
-				return "x = " +a+" * "+ neg_angls[0] +a+" * "+ "pi*n, n=Z";
-			} else if (b == -1) {
+			return "x = " + a + " * " + angls[2] + a + " * " + "pi*n, n=Z";
+		} else if (b == -(Math.sqrt(3))) {
 
-				return "x = " +a+" * "+ neg_angls[5] +a+" * "+ "pi*n, n=Z";
-			} else if (b > COUNT || b < -COUNT) {
+			return "x = " + a + " * " + neg_angls[2] + a + " * " + "pi*n, n=Z";
+		} else if (b == -(1 / (Math.sqrt(3)))) {
 
-				return "does not exist";
-			}
-			return "ERROR";
-		}	
+			return "x = " + a + " * " + neg_angls[0] + a + " * " + "pi*n, n=Z";
+		} else if (b == -1) {
+
+			return "x = " + a + " * " + neg_angls[5] + a + " * " + "pi*n, n=Z";
+		} else if (b > COUNT || b < -COUNT) {
+
+			return "does not exist";
+		}
+		return "ERROR";
+	}
+
+	// Ctg(x/a)=b
+	public String solvCtg_3(double a, double b) {
+
+		if (b == 1 / (Math.sqrt(3))) {
+
+			return "x = "+a+"*" + angls[2] +" + "+a+ " * pi*n, n=Z";
+		} else if (b == 1) {
+
+			return "x = "+a+"*" +  angls[1] +" + "+a+ " * pi*n, n=Z";
+		} else if (b == Math.sqrt(3)) {
+
+			return "x = " +a+"*" +  angls[0] +" + "+a+ " * pi*n, n=Z";
+		} else if (b == -(Math.sqrt(3))) {
+
+			return "x = " +a+"*" +  neg_angls[4] +" + "+a+ " * pi*n, n=Z";
+		} else if (b == -(1 / (Math.sqrt(3)))) {
+
+			return "x = " +a+"*" +  neg_angls[6] +" + "+a+ " * pi*n, n=Z";
+		} else if (b == -1) {
+
+			return "x = " +a+"*" + neg_angls[5] +" + "+a+ " * pi*n, n=Z";
+		} else if (b > COUNT || b < -COUNT) {
+			return "does not exist";
+		}
+		return "ERROR";
+	}
 
 }
