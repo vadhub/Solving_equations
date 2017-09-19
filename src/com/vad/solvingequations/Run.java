2f6e0ed60cb365a1dc85f05ccdf5ba948a44d640
vadim <gabderahmanov99@gmail.com>
2017-09-19 20:20:59 +0400

    change mathOperations method

--- src/com/vad/solvingequations/Run.java
+++ src/com/vad/solvingequations/Run.java
@@ -13,8 +13,8 @@ public class Run {
 	public static void main(String[] args) {
 		Equations equations = new Equations();
 		EquationsTrigeometry equationsTri = new EquationsTrigeometry();
-		MappingSolutions mappingSol = new MappingSolutions();		
-			
+		MappingSolutions mappingSol = new MappingSolutions();
+
 		// gui bloc
 
 		JFrame frame = new JFrame();
@@ -57,7 +57,6 @@ public class Run {
 		ch.add("ctg(x/a) = b");
 		ch.add("a*sin(x)^2+b*sin(x)+c=0");
 		ch.add("a*cos(x)^2+b*cos(x)+c=0");
-				
 
 		JTextField a = new JTextField(4);
 		JTextField b = new JTextField(4);
@@ -107,22 +106,22 @@ public class Run {
 
 				if (ch.getSelectedIndex() == 0) {
 					double x = equations.solvSum(int_a, int_b);
-					
-					mappingSol.DisplayForSum(int_a, int_b, x);	
-					
+
+					mappingSol.DisplayForSum(int_a, int_b, x);
+
 					result.setText(String.valueOf(x));
 				} else if (ch.getSelectedIndex() == 1) {
-					
+
 					double x = equations.solvR_X1(int_a, int_b, int_c);
-					double x2 = equations.solvR_X2( int_a, int_b, int_c);
-					
+					double x2 = equations.solvR_X2(int_a, int_b, int_c);
+
 					mappingSol.DisplayForR(int_a, int_b, int_c);
 
-					result.setText(String.valueOf(x)+";"+String.valueOf(x2));
+					result.setText(String.valueOf(x) + ";" + String.valueOf(x2));
 
 				} else if (ch.getSelectedIndex() == 2) {
 
-					double x = equations.solvFra( int_a, int_b);
+					double x = equations.solvFra(int_a, int_b);
 
 					result.setText(String.valueOf(x));
 
@@ -135,7 +134,7 @@ public class Run {
 				} else if (ch.getSelectedIndex() == 4) {
 
 					double x = equations.solvFra3(int_a, int_b, int_c);
-					
+
 					mappingSol.DisplayForFra3((int) int_a, int_b, int_c);
 
 					result.setText(String.valueOf(x));
@@ -143,180 +142,189 @@ public class Run {
 				} else if (ch.getSelectedIndex() == 5) {
 
 					double x = equations.solvMul(int_a, int_b);
-					
+
 					mappingSol.DisplaysolvMul((int) int_a, int_b, x);
 					result.setText(String.valueOf(x));
 
 				} else if (ch.getSelectedIndex() == 6) {
 
-					double x = equations.solvFra4((int) int_a, int_b, int_c, int_d, int_e);
+					double x = equations.solvFra4((int) int_a, int_b, int_c,
+							int_d, int_e);
 
 					result.setText(String.valueOf(x));
 
 				} else if (ch.getSelectedIndex() == 7) {
 
-					double x = equations.solvFra5((int) int_a, int_b, int_c, int_d);
-					
-					mappingSol.DisplaysolvFra_5((int) int_a, int_b, int_c, int_d, x);
+					double x = equations.solvFra5((int) int_a, int_b, int_c,
+							int_d);
+
+					mappingSol.DisplaysolvFra_5((int) int_a, int_b, int_c,
+							int_d, x);
 
 					result.setText(String.valueOf(x));
-					
+
 				} else if (ch.getSelectedIndex() == 8) {
-					
+
 					String x = equationsTri.solvSin(int_a);
-					
+
 					mappingSol.DisplaySolvSin(int_a);
-					
+
 					result.setText(x);
 
 				} else if (ch.getSelectedIndex() == 9) {
 
-					String x = equationsTri.solvCos( int_a);
-					
+					String x = equationsTri.solvCos(int_a);
+
 					mappingSol.DisplaySolvCos(int_a);
-					
+
 					result.setText(x);
-					
+
 				} else if (ch.getSelectedIndex() == 10) {
 
-					String x = equationsTri.solvTg( int_a);
-					
-					mappingSol.DisplaySolvTg( int_a);
-					
+					String x = equationsTri.solvTg(int_a);
+
+					mappingSol.DisplaySolvTg(int_a);
+
 					result.setText(x);
-					
+
 				} else if (ch.getSelectedIndex() == 11) {
 
-					double x = equations.solvSqrt_2( int_a, int_b);
-					
-					mappingSol.DisplaysolvSqrt_2( int_a, int_b, x);					
+					double x = equations.solvSqrt_2(int_a, int_b);
+
+					mappingSol.DisplaysolvSqrt_2(int_a, int_b, x);
 					result.setText(String.valueOf(x));
-					
+
 				} else if (ch.getSelectedIndex() == 12) {
 
-					double x = equations.solvSqrt_3( int_a, int_b);
+					double x = equations.solvSqrt_3(int_a, int_b);
 					result.setText(String.valueOf(x));
-					
+
 				} else if (ch.getSelectedIndex() == 13) {
 
-					double x = equations.solvSqrt_4( int_a, int_b);
+					double x = equations.solvSqrt_4(int_a, int_b);
 					result.setText(String.valueOf(x));
-					
+
 				} else if (ch.getSelectedIndex() == 14) {
 
-					double x1 = equations.solvMod_X1( int_a, int_b);
-					double x2 = equations.solvMod_X2( int_a, int_b);
-					
-					mappingSol.DisplaySolvMod(int_a, int_b, x1,x2);
-					
-					result.setText(String.valueOf(x1) + " ; " + String.valueOf(x2));
-				} else if(ch.getSelectedIndex() == 15){
-					
-					String x = equationsTri.solvCtg( int_a);
-					
-					mappingSol.DisplaySolvCtg( int_a);
-					
+					double x1 = equations.solvMod_X1(int_a, int_b);
+					double x2 = equations.solvMod_X2(int_a, int_b);
+
+					mappingSol.DisplaySolvMod(int_a, int_b, x1, x2);
+
+					result.setText(String.valueOf(x1) + " ; "
+							+ String.valueOf(x2));
+				} else if (ch.getSelectedIndex() == 15) {
+
+					String x = equationsTri.solvCtg(int_a);
+
+					mappingSol.DisplaySolvCtg(int_a);
+
 					result.setText(x);
-					
-				} else if(ch.getSelectedIndex() == 16){
-					
+
+				} else if (ch.getSelectedIndex() == 16) {
+
 					String x = equationsTri.solvSin_2(int_a, int_b);
-					mappingSol.DisplaySolvSin_2( int_a, int_b);
-					
+					mappingSol.DisplaySolvSin_2(int_a, int_b);
+
 					result.setText(x);
-				} else if(ch.getSelectedIndex() == 17){
-					
+				} else if (ch.getSelectedIndex() == 17) {
+
 					String x = equationsTri.solvCos_2(int_a, int_b);
 					mappingSol.DisplaySolvCos_2(int_a, int_b);
-					
+
 					result.setText(x);
-				} else if(ch.getSelectedIndex() == 18){
-					
+				} else if (ch.getSelectedIndex() == 18) {
+
 					String x = equationsTri.solvTg_2(int_a, int_b);
-					mappingSol.DisplaySolvTg_2(int_a, int_b);					
-					
+					mappingSol.DisplaySolvTg_2(int_a, int_b);
+
 					result.setText(x);
-				} else if(ch.getSelectedIndex() == 19){
-					
+				} else if (ch.getSelectedIndex() == 19) {
+
 					double x = equations.solvSystemEnqX(int_a, int_b);
-					double y = equations.solvSystemEnqY( int_a, int_b);
-					
-					result.setText(String.valueOf(x)+";"+String.valueOf(y));
-				}	else if(ch.getSelectedIndex() == 20){
-					
+					double y = equations.solvSystemEnqY(int_a, int_b);
+
+					result.setText(String.valueOf(x) + ";" + String.valueOf(y));
+				} else if (ch.getSelectedIndex() == 20) {
+
 					String x = equationsTri.solvSin_3(int_a, int_b);
 					mappingSol.DisplaySolvSin_3(int_a, int_b);
-					
+
 					result.setText(x);
-				}	else if(ch.getSelectedIndex() == 21){
-					
+				} else if (ch.getSelectedIndex() == 21) {
+
 					String x = equationsTri.solvCos_3(int_a, int_b);
 					mappingSol.DisplaySolvCos_3(int_a, int_b);
-					
+
 					result.setText(x);
-				}else if(ch.getSelectedIndex() == 22){
-					
+				} else if (ch.getSelectedIndex() == 22) {
+
 					String x = equationsTri.solvTg_3(int_a, int_b);
 					mappingSol.DisplaySolvTg_3(int_a, int_b);
-					
+
 					result.setText(x);
-				}else if(ch.getSelectedIndex() == 23){
-					
+				} else if (ch.getSelectedIndex() == 23) {
+
 					String x = equationsTri.solvCtg_3(int_a, int_b);
 					mappingSol.DisplaySolvCtg_3(int_a, int_b);
-					
+
 					result.setText(x);
-				}else if(ch.getSelectedIndex() == 24){
-					
+				} else if (ch.getSelectedIndex() == 24) {
+
 					String x = equationsTri.solvSin_X(int_a, int_b, int_c);
-					
+
 					mappingSol.DisplaySolvSin_X(int_a, int_b, int_c);
 					result.setText(x);
-				}else if(ch.getSelectedIndex() == 25){
-					
+				} else if (ch.getSelectedIndex() == 25) {
+
 					String x = equationsTri.solvCos_X(int_a, int_b, int_c);
-					
+
 					mappingSol.DisplaySolvCos_X(int_a, int_b, int_c);
 					result.setText(x);
-				}						
-				
+				}
+
 			}
 		});
-		
-	System.out.println(deleteSymbol("8-x=9"));
-	mathOperations("2+8-x=9");
-	
+
+		System.out.println(deleteSymbol("10-x=9"));
+		mathOperations("10-x=9");
+
 	}
-	
-	public static ArrayList<Integer> deleteSymbol(String str){     
-	       ArrayList<Integer> inm = new ArrayList<Integer>();       
-	       int in = 0;	      
-	       for (int i = 0;i<str.length();i++){ 
-				if (Character.isDigit(str.charAt(i))) {					
-					in = Character.getNumericValue(str.charAt(i));
-					inm.add(in);
-				}				 
-	       }	       
-	       return inm;
-	       
+
+	public static ArrayList<Integer> deleteSymbol(String str) {
+		ArrayList<Integer> inm = new ArrayList<Integer>();
+		int in = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isDigit(str.charAt(i))) {
+				in = Character.getNumericValue(str.charAt(i));
+				inm.add(in);
+			}
+		}
+		return inm;
+
 	}
-	public static boolean detectSymbol(String str, String s){
-		return str.contains(s);		
+
+	public static boolean detectSymbol(String str, String s) {
+		return str.contains(s);
 	}
-	
-	public static void mathOperations(String str){
+
+	public static void mathOperations(String str) {
 		int o = 0;
-		if(detectSymbol(str, "+")==true){
-			for(int i = 0;i<deleteSymbol(str).size();i++){				
-				o =o + deleteSymbol(str).get(i);				
-			}			
+		if (detectSymbol(str, "+") == true) {
+			for (int i = 0; i < deleteSymbol(str).size(); i++) {
+				if(detectSymbol(str, "=") == false){
+					o = o + deleteSymbol(str).get(i);
+				}else{
+					o = deleteSymbol(str).get(i)-o;
+				}			
+			}
 		}
 		System.out.println(o);
-		if(detectSymbol(str, "-")==true){
-			for(int i = 0;i<deleteSymbol(str).size();i++){
-				if(deleteSymbol(str).get(i)>deleteSymbol(str).get(0))
-				o = deleteSymbol(str).get(i)-deleteSymbol(str).get(i-1);				
-			}			
+		if (detectSymbol(str, "-") == true) {
+			for (int i = 0; i < deleteSymbol(str).size(); i++) {
+				if (deleteSymbol(str).get(i) > deleteSymbol(str).get(0))
+					o = deleteSymbol(str).get(i) - deleteSymbol(str).get(i - 1);
+			}
 		}
 		System.out.println(o);
 	}
