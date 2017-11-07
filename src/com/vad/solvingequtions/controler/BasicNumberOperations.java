c5e12611e266dd24f8dca5e85b236fdbd34c2ab9
vadim <gabderahmanov99@gmail.com>
2017-11-07 19:17:03 +0400

    chekstyle

--- src/com/vad/solvingequtions/controler/BasicNumberOperations.java
+++ src/com/vad/solvingequtions/controler/BasicNumberOperations.java
@@ -7,22 +7,21 @@ public class BasicNumberOperations {
 		return a * a;
 	}
 
-	public String deleteSpace(String s){
+	public String deleteSpace(String s) {
 		s = s.replace(" ", "");
 		return s;
 	}
-	
-	//adding numbers into arraylist
-	public ArrayList<Double> addInt(String ast, ArrayList<Double> intList){
-		
+
+	// adding numbers into arraylist
+	public ArrayList<Double> addInt(String ast, ArrayList<Double> intList) {
+
 		int pass = ast.indexOf("=");
 
 		if (pass == -1) {
 		} else {
 			ast = ast.replace('=', '-');
-		}		
-		
-		
+		}
+
 		String[] arrStirng = ast.split(" ");
 
 		for (String s : arrStirng) {
@@ -34,10 +33,10 @@ public class BasicNumberOperations {
 		}
 
 		return intList;
-		
+
 	}
-	
-	public ArrayList<Double> squreNum(String ast, ArrayList<Double> sqList){
+
+	public ArrayList<Double> squreNum(String ast, ArrayList<Double> sqList) {
 		String[] arrStirng = ast.split(" ");
 
 		for (String s : arrStirng) {
@@ -49,80 +48,82 @@ public class BasicNumberOperations {
 		}
 
 		return sqList;
-		
+
 	}
 
 	// current x
 	public int numX(String str) {
 		int num = 0;
 		int numSub = 0;
-		
+
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
 
 			if (c == 'x') {
 
 				if (Character.isDigit(str.charAt(i - 1))) {
-					numSub = numSub	+ Character.getNumericValue(str.charAt(i - 1));
+					numSub = numSub
+							+ Character.getNumericValue(str.charAt(i - 1));
 				} else {
 					num++;
 				}
 			}
-		}			
+		}
 		return numSub + num;
 	}
-	// delete '0'
-	public ArrayList<Double> deleteZ(ArrayList<Double> intList){
-		
-	for (int i = 0; i < intList.size(); i++) {
 
-		if (intList.get(i) == 0) {
-			intList.remove(i);
+	// delete '0'
+	public ArrayList<Double> deleteZ(ArrayList<Double> intList) {
+
+		for (int i = 0; i < intList.size(); i++) {
+
+			if (intList.get(i) == 0) {
+				intList.remove(i);
+			}
 		}
+		return intList;
 	}
-	return intList;
-}
 
 	// detect symbols in string
-		public ArrayList<Double> detectInteger(String str) {
-			deleteSpace(str);
-			ArrayList<Double> intList = new ArrayList<Double>();
+	public ArrayList<Double> detectInteger(String str) {
+		deleteSpace(str);
+		ArrayList<Double> intList = new ArrayList<Double>();
 
-			String ast = "";
+		String ast = "";
 
-			for (int i = 0; i < str.length(); i++) {
+		for (int i = 0; i < str.length(); i++) {
 
-				char c = str.charAt(i);
-				if (c == '/') {
-					ast += " " + c;
-				} else if (c == '-') {
-					ast += " " + c;
-				} else if (c == '+') {
-					ast += " " + c;
-				} else if (c == '=') {
-					ast += " " + c;				
-				}else {
-					ast += c;
-				}
-			}			
-			System.out.println(ast);
-			addInt(ast, intList);
-			deleteZ(intList);
-
-			return intList;
+			char c = str.charAt(i);
+			if (c == '/') {
+				ast += " " + c;
+			} else if (c == '-') {
+				ast += " " + c;
+			} else if (c == '+') {
+				ast += " " + c;
+			} else if (c == '=') {
+				ast += " " + c;
+			} else {
+				ast += c;
+			}
 		}
+		System.out.println(ast);
+		addInt(ast, intList);
+		deleteZ(intList);
+
+		return intList;
+	}
 
 	// fulfill math operations
 	public double mathOperations(String str1) {
 		double summ = 0;
-		
+
 		ArrayList<Double> listNumbers = detectInteger(str1);
-		
+
 		for (int i = 0; i < listNumbers.size(); i++) {
 			summ = summ + listNumbers.get(i);
-		}	
-		
-		try {			
+		}
+
+		try {
 			return (summ * -1) / numX(str1);
 		} catch (Exception ex) {
 			return summ;
